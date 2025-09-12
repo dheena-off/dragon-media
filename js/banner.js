761c24afc4ff753f5a8e0bccacc4ed3507fbf4c4
dheena-off <dheenaofficial181@gmail.com>
2025-09-12 11:54:03 +0530

    first commit

---
+++ js/banner.js
@@ -0,0 +1,7 @@
+//  document.addEventListener("DOMContentLoaded", () => {
+//    const el = document.querySelector(".banner"); // <-- must match HTML
+//    if (el) {
+//       console.log("Banner found:", el);
+//       ScrollReveal().reveal('.banner', { delay: 200 });
+//    }
+// });
